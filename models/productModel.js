ea3e62ce0c5271570f06395330311aee5ec048ba
minhduc <64589567+leminhduc106@users.noreply.github.com>
2021-11-28 18:34:46 +0700

    Add models

---
+++ models/productModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+//const slugify = require('slugify');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  slug: {
+    type: String
+  },
+  detail: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  nameImage: {
+    type: String
+  },
+  size: {
+    type: String
+  },
+  color:{
+    type: String
+  }
+
+});
+
+const Product = mongoose.model('Product', productSchema, "products");
+
+module.exports = Product;
