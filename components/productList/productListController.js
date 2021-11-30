519b061a67ec8018988c02b13ed47d8b7ba2aa44
Catelt <54297602+Catelt@users.noreply.github.com>
2021-11-30 12:06:08 +0700

    cloud

--- components/productList/productListController.js
+++ components/productList/productListController.js
@@ -1,6 +1,16 @@
 const productListService = require('./productListService');
 const Product = require("../../models/productModel");
 
+
+const cloudinary = require('cloudinary').v2;
+const formidable = require('formidable');
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET
+  });
+
 exports.list = async function(req, res) {
     const products = await productListService.list();
     res.render('productList/productlist', {products});
@@ -11,19 +21,25 @@ exports.delete = async function(req, res) {
 
     Product.findByIdAndDelete(id)
         .then(result => {
+            cloudinary.uploader.destroy(`images/${result._id}/${result.nameImage}`,function(){
+                cloudinary.api.delete_folder(`images/${result._id}`, console.log);
+            })
             res.redirect('/productlist');
         })
         .catch(err => console.log(err));
 };
 
 exports.add = async function(req, res) {
-    const product = new Product(req.body);
-
-    product.save()
+    const form = formidable();
+    form.parse(req,(err,fields,files)=>{
+        product = new Product(fields);
+        product.save()
         .then((result) => {
+            cloudinary.uploader.upload(files.image.filepath, { public_id: `images/${result._id}/${result.nameImage}` })
             res.redirect('/productlist');
-    })
+        })
         .catch(err => console.log(err));
+    })
 };
 
 exports.edit = function(req, res) {
