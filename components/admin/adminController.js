b04a9a98eaf064ae6021dfde57fc645317f51a9d
Catelt <54297602+Catelt@users.noreply.github.com>
2021-12-16 10:46:34 +0700

    add css html productlist

--- components/admin/adminController.js
+++ components/admin/adminController.js
@@ -1,7 +1,7 @@
 const adminListService = require('./adminService');
 
 exports.list = async function(req, res) {
-    const perPage = 3;
+    const perPage = 6;
     const page = req.params.page || 1;
     const count = await adminListService.count();
     const admins = await adminListService.list(page,perPage);
