a726dd5ac5ae4bb1e30b773d4bd9e6203cc5b585
thientoan0101 <thientoancubi@gmail.com>
2021-12-22 11:27:10 +0700

    analysis on month, year, season

--- components/analysis/analysisRouter.js
+++ components/analysis/analysisRouter.js
@@ -6,6 +6,14 @@ const analysisController = require('./analysisController');
 
 router.get('/', analysisController.getTotalOrderInOneMonth);
 
+router.get('/day', analysisController.getTotalOrderInOneMonth);
+
+router.get('/month', analysisController.getTotalOrderIn12Month);
+
+router.get('/year', analysisController.getTotalOrderIn4Year);
+
+router.get('/season', analysisController.getTotalOrderInSeason);
+
 router.get('/test', analysisController.test);
 module.exports = router;
 
